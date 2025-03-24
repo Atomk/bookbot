6b575cdd5bb8aa9a67c0dc8d624d15c4a7debf47
Atomk <marco.fras@hotmail.it>
2025-03-24 03:54:43 +0100

    feat: character frequency data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, character_frequency
 
 def get_book_text(filepath) -> str:
     with open(filepath) as f:
@@ -9,5 +9,8 @@ def main():
     frankenstein = get_book_text("./books/frankenstein.txt")
     num_words = count_words(frankenstein)
     print(f"{num_words} words found in the document")
+
+    freq_dict = character_frequency(frankenstein)
+    print(freq_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,15 @@
 def count_words(text: str) -> int:
     return len(text.split())
+
+
+def character_frequency(text: str) -> dict[str, int]:
+    chars_dict = {}
+
+    for char in text:
+        char = char.lower()
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+
+    return chars_dict
