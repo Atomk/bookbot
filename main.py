b5a861410fd31633b75c6a2f48f4584e273f8db6
Atomk <marco.fras@hotmail.it>
2025-03-24 03:51:42 +0100

    refactor: move stats functions

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import count_words
+
 def get_book_text(filepath) -> str:
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-
-def count_words(text: str) -> int:
-    return len(text.split())
 
 def main():
     frankenstein = get_book_text("./books/frankenstein.txt")
